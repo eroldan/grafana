3b20e948f1ce9c06904f8898789d41005e853247
Kristina Demeshchik <kristina.demeshchik@gmail.com>
2026-03-10 14:47:56 -0400

    Add filters to navigation controls

--- public/app/features/dashboard-scene/edit-pane/add-new/AddNewEditPane.tsx
+++ public/app/features/dashboard-scene/edit-pane/add-new/AddNewEditPane.tsx
@@ -4,6 +4,7 @@ import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 import { GrafanaTheme2 } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
 import { t } from '@grafana/i18n';
+import { config } from '@grafana/runtime';
 import { SceneObject } from '@grafana/scenes';
 import { Sidebar, useStyles2 } from '@grafana/ui';
 import { getLayoutType } from 'app/features/dashboard/utils/tracking';
@@ -13,6 +14,7 @@ import { DashboardInteractions } from '../../utils/interactions';
 import { getDashboardSceneFor } from '../../utils/utils';
 
 import { AddAnnotationQuery } from './AddAnnotationQuery';
+import { AddFilters } from './AddFilters';
 import { AddLink } from './AddLink';
 import { AddNewSection } from './AddNewSection';
 import { AddVariable } from './AddVariable';
@@ -81,6 +83,7 @@ export function AddNewEditPane({ onAddPanel, dashboard, selectedElement }: AddNe
         </DragDropContext>
       </AddNewSection>
       <AddNewSection title={t('dashboard-scene.dashboard-side-pane-new.dashboard-controls', 'Dashboard controls')}>
+        {config.featureToggles.dashboardUnifiedDrilldownControls && <AddFilters dashboardScene={dashboardScene} />}
         <AddVariable dashboardScene={dashboardScene} />
         <AddAnnotationQuery dashboardScene={dashboardScene} />
         <AddLink dashboardScene={dashboardScene} />
